9935cda427783d35d4b050889387ba7a05906a86
Rémi Prévost <remi@exomel.com>
2016-04-17 17:40:21 -0400

    Implement “lab bro” shell command just to open the repository on GitLab

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -21,3 +21,11 @@ function grepkill() {
 function grepkill9() {
   ps aux | grep -e $1 | grep -v grep | awk '{print $2}' | xargs -p kill -9
 }
+
+function lab() {
+  case $1 in
+    bro)
+        url=$(git remote -vv | grep "githost" | head -n1 | sed 's/:/\//' | sed 's/.*git@\([^ ]*\).*/https:\/\/\1/')
+        open "$url"
+  esac
+}
