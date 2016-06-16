bdc0a5427ce49f3030032ac5f04d8b4d6d325a94
Rémi Prévost <remi@exomel.com>
2016-06-16 06:22:34 -0400

    Remove rbenv-prompt because we can

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -7,9 +7,9 @@ function collapse_pwd() {
 }
 
 # Print the prompt
-# `rbenv-prompt` and `git-prompt` are in ../bin/
+# `git-prompt` is in ../bin/
 function generate_prompt() {
-  echo "$PR_NO_COLOR$PR_LIGHT_YELLOW$(collapse_pwd)$PR_NO_COLOR$PR_LIGHT_WHITE$(rbenv-prompt)$(git-prompt)$PR_NO_COLOR\n$PR_GREEN%%$PR_NO_COLOR "
+  echo "$PR_NO_COLOR$PR_LIGHT_YELLOW$(collapse_pwd)$PR_NO_COLOR$PR_LIGHT_WHITE$(git-prompt)$PR_NO_COLOR\n$PR_GREEN%%$PR_NO_COLOR "
 }
 
 # Kill via grep
