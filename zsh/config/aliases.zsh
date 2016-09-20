41ee2e5440649b16f6497f4b9c910bead0ec06d2
Rémi Prévost <remi@exomel.com>
2016-09-20 18:07:46 -0400

    Clean a few zsh things

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -2,14 +2,11 @@
 # --------------------------------------
 alias ls="ls -hFG --color=always --group-directories-first"
 alias ll='ls -la'
-alias la='ls -A'
 alias l='ls -l'
 alias rm='rm -i'
-alias f='find . -iname'
 alias p='ps -ef | ack -i '
 alias listen="sudo lsof -i -P"
 alias h="heroku"
-alias ack="ag"
 alias ..='cd ..'
 alias ~='cd ~'
 
@@ -20,8 +17,6 @@ alias vim='nvim'
 # Ruby
 alias b="bundle"
 alias be="bundle exec "
-alias rt="RAILS_ENV=test"
-alias sk="SKIP_CODE_CHECK=1"
 
 # Tmux
 alias tmux="TMPDIR=$HOME/.tmux tmux"
@@ -33,12 +28,3 @@ alias redis-daemon="redis-server `brew --prefix`/etc/redis.conf"
 alias elasticsearch-daemon="elasticsearch -d es.config=`brew --prefix elasticsearch`/config/elasticsearch.yml"
 alias pg-daemon="pg_ctl -D `brew --prefix`/var/postgres -l `brew --prefix`/var/postgres/server.log start"
 alias php-daemon="`brew --prefix php56`/sbin/php-fpm --fpm-config `brew --prefix`/etc/php/5.6/php-fpm.conf"
-
-# Global aliases
-# --------------------------------------
-alias -g L="| less"
-alias -g SORT='| sort'
-alias -g LC='| wc -l'
-alias -g A='| ack -i'
-alias -g AA='| ack'
-alias -g BG='> /dev/null 2>&1 &'
