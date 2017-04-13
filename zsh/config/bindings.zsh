4c95e7b2debb2d4da1f714e889ad6203b6885c77
Rémi Prévost <remi@exomel.com>
2017-04-13 06:25:20 -0400

    Set KEYTIMEOUT in zsh

--- zsh/config/bindings.zsh
+++ zsh/config/bindings.zsh
@@ -4,3 +4,7 @@ bindkey -v # Vim mode!
 bindkey "^r" history-incremental-search-backward
 bindkey "^w" history-incremental-search-forward
 bindkey ' ' magic-space
+
+# https://github.com/pda/dotzsh/blob/b9be1262ceff553ed0c9d254c29a5a950cace85b/keyboard.zsh#L4-L10
+# --------------------------------------
+KEYTIMEOUT=1
