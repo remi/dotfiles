9a48b00871420137823706c9922d92e5d6fbc9af
Rémi Prévost <remi@exomel.com>
2019-02-06 14:17:20 -0500

    [zsh] Improve syntax highlight patterns

--- zsh/config/syntax-highlighting.zsh
+++ zsh/config/syntax-highlighting.zsh
@@ -1,8 +1,10 @@
 # Syntax highlighting
 # --------------------------------------
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
-ZSH_HIGHLIGHT_PATTERNS+=('rm -fr*' 'fg=white,bold,bg=red,bold')
-ZSH_HIGHLIGHT_PATTERNS+=('rm -rf*' 'fg=white,bold,bg=red,bold')
-ZSH_HIGHLIGHT_PATTERNS+=('git push -f*' 'fg=white,bold,bg=red,bold')
-ZSH_HIGHLIGHT_PATTERNS+=('git pf*' 'fg=white,bold,bg=red,bold')
-ZSH_HIGHLIGHT_PATTERNS+=('git co' 'fg=white,bold,bg=cyan,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('rm -fr *' 'fg=white,bold,bg=red,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('git push -f *' 'fg=white,bold,bg=red,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('git push --force *' 'fg=white,bold,bg=red,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('git pf *' 'fg=white,bold,bg=red,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('git co .' 'fg=white,bold,bg=red,bold')
+ZSH_HIGHLIGHT_PATTERNS+=('git co *' 'fg=white,bold,bg=cyan,bold')
