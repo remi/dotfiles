e1d0a1f71c1dae0958556d12f31ec38dc316b836
Rémi Prévost <remi@exomel.com>
2023-07-18 15:29:15 -0400

    Add Helpers.copy/1 helper

--- iex/iex.exs
+++ iex/iex.exs
@@ -1,3 +1,20 @@
 IEx.configure(colors: [eval_result: [:red, :bright]])
 IEx.configure(inspect: [limit: 10])
 IEx.configure(history_size: 100)
+
+defmodule Helpers do
+  def copy(term) do
+    text =
+      if is_binary(term) do
+        term
+      else
+        inspect(term, limit: :infinity, pretty: true)
+      end
+
+    port = Port.open({:spawn, "pbcopy"}, [])
+    true = Port.command(port, text)
+    true = Port.close(port)
+
+    :ok
+  end
+end
