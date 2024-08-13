36a810a341ac9b49a4c07ce95ba0151d05277df9
Rémi Prévost <remi@exomel.com>
2024-08-13 15:20:08 -0400

    [wezterm] Customize cursor

--- wezterm/werterm.lua
+++ wezterm/werterm.lua
@@ -7,6 +7,11 @@ config.font_size = 17.0
 config.color_scheme = 'Twilight (dark) (terminal.sexy)'
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
+config.cursor_thickness = '10px'
+config.default_cursor_style = 'BlinkingBlock'
+config.animation_fps = 1
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
 
 -- Tabs
 config.enable_tab_bar = false
