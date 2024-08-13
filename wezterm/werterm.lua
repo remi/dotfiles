4399a606f662c012612658d19c53a5ead88ac1cc
Rémi Prévost <remi@exomel.com>
2024-08-13 15:05:12 -0400

    [wezterm] Add WezTerm config + related changes for terminal-based tools

---
+++ wezterm/werterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- Appearance
+config.font = wezterm.font 'VictorMono Nerd Font'
+config.font_size = 17.0
+config.color_scheme = 'Twilight (dark) (terminal.sexy)'
+-- config.window_background_opacity = 0.9
+-- config.macos_window_background_blur = 20
+
+-- Tabs
+config.enable_tab_bar = false
+
+-- Keys
+config.disable_default_key_bindings = true
+config.keys = {
+  { key = 'Enter', mods = 'CMD', action = wezterm.action.ToggleFullScreen },
+  { key = 'q', mods = 'CMD', action = wezterm.action.QuitApplication },
+  { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'v', mods = 'SUPER', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'Copy', mods = 'NONE', action = wezterm.action.CopyTo 'Clipboard' },
+  { key = 'Paste', mods = 'NONE', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'c', mods = 'SHIFT|CTRL', action = wezterm.action.CopyTo 'Clipboard' },
+  { key = 'c', mods = 'SUPER', action = wezterm.action.CopyTo 'Clipboard' },
+}
+
+return config
