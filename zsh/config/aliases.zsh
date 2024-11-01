9c3f322f65226d3c5c8dcd17ad43b6fd768af626
Rémi Prévost <remi@exomel.com>
2024-11-01 11:17:16 -0400

    [zsh] Use eza, no more exa

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,13 +1,13 @@
 # Local aliases
 # --------------------------------------
-alias ls="exa --long --time-style=long-iso --git --group-directories-first --header --links"
+alias ls="eza --long --time-style=long-iso --git --group-directories-first --header --links"
 alias ll="ls -la"
 alias l="ls -l"
 alias rm="rm -i"
 alias p="ps -ef | rg -i "
 alias ..="cd .."
 alias ~="cd ~"
-alias tree="exa -T"
+alias tree="eza -T"
 alias cat="bat"
 alias ssh="TERM=${TERM/tmux/xterm} ssh"
 alias gh="op plugin run -- gh"
