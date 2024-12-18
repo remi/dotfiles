8f08e1248a6e38a497133a83e2dbfa2d08ffee15
Rémi Prévost <remi@exomel.com>
2024-12-17 20:28:05 -0500

    [vim] Improve autoformat a little bit more

--- vim/lua/plugins/formatting.lua
+++ vim/lua/plugins/formatting.lua
@@ -10,28 +10,47 @@ return {
         typescript = { "prettierd" },
         javascriptreact = { "prettierd" },
         typescriptreact = { "prettierd" },
-        svelte = { "prettierd" },
         css = { "prettierd" },
-        html = { "prettierd" },
         json = { "prettierd" },
-        yaml = { "prettierd" },
         markdown = { "prettierd" },
         graphql = { "prettierd" },
         lua = { "stylua" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 2000,
-      },
+
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 2000, lsp_format = "fallback" }
+      end,
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>ff", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 2000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+
+      print("Autoformat-on-save disabled")
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+
+      print("Autoformat-on-save enabled")
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>fe", ":FormatEnable<CR>", opts)
+    vim.keymap.set("n", "<leader>fd", ":FormatDisable<CR>", opts)
   end,
 }
