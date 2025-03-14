1191385c04f6c02d1a8742c007a1a30a591dd990
Rémi Prévost <remi@exomel.com>
2025-03-14 13:44:07 -0400

    [vim] Add more treesitter plugins

--- vim/lua/plugins/nvim-treesitter.lua
+++ vim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,19 @@ return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "markdown", "markdown_inline", "query", "elixir", "typescript", "ruby" },
+      ensure_installed = {
+        "c",
+        "elixir",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "ruby",
+        "ssh_config",
+        "typescript",
+        "vim",
+        "vimdoc",
+      },
       auto_install = true,
       highlight = {
         enable = true,
