5051798ae570ee15eb8b942f7170296c5e86c3ae
Rémi Prévost <remi@exomel.com>
2025-05-04 20:13:31 -0400

    [vim] Use cursor as default editor (I know, I know…)

--- zsh/config/variables.zsh
+++ zsh/config/variables.zsh
@@ -32,8 +32,8 @@ export SAVEHIST=1000000
 # Utilities
 export PAGER='less'
 export DELTA_PAGER='less -R'
-export EDITOR='nvim'
-export GIT_EDITOR='nvim'
+export EDITOR='cursor -w'
+export GIT_EDITOR='cursor -w'
 export BROWSER='open'
 export LESS="-+e -+f -R -+c -X -M -S -r"
 
