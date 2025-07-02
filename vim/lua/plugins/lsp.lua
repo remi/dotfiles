0044ff2d045e005d9a729f6c9704e7f930eaefee
Rémi Prévost <remi@exomel.com>
2025-07-01 22:34:54 -0400

    [vim] Fix Neovim deprecated vim.fn.sign_define usage

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -29,22 +29,17 @@ return {
 
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
-          -- Configure signs
-          local signs = {
-            { name = "DiagnosticSignError", text = "" },
-            { name = "DiagnosticSignWarn", text = "" },
-            { name = "DiagnosticSignHint", text = "" },
-            { name = "DiagnosticSignInfo", text = "" },
-          }
-
-          for _, sign in ipairs(signs) do
-            vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-          end
-
           -- Configure diagnostics
           vim.diagnostic.config({
             virtual_text = true,
-            signs = { active = signs },
+            signs = {
+              text = {
+                [vim.diagnostic.severity.ERROR] = '',
+                [vim.diagnostic.severity.WARN] = '',
+                [vim.diagnostic.severity.INFO] = '',
+                [vim.diagnostic.severity.HINT] = '',
+              },
+            },
             update_in_insert = true,
             underline = true,
             severity_sort = true,
